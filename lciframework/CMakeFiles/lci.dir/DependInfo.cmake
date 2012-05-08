ca2b9259237ac8c9dca9664b2b2a7dd5816ed531
Eric Gallager <egallager@comcast.net>
2012-05-08 16:27:21 -0400

    Build OS X framework version

---
+++ lciframework/CMakeFiles/lci.dir/DependInfo.cmake
@@ -0,0 +1,18 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/Users/ericgallager/lci/lciframework/interpreter.c" "/Users/ericgallager/lci/lciframework/CMakeFiles/lci.dir/interpreter.c.o"
+  "/Users/ericgallager/lci/lciframework/lexer.c" "/Users/ericgallager/lci/lciframework/CMakeFiles/lci.dir/lexer.c.o"
+  "/Users/ericgallager/lci/lciframework/main.c" "/Users/ericgallager/lci/lciframework/CMakeFiles/lci.dir/main.c.o"
+  "/Users/ericgallager/lci/lciframework/parser.c" "/Users/ericgallager/lci/lciframework/CMakeFiles/lci.dir/parser.c.o"
+  "/Users/ericgallager/lci/lciframework/tokenizer.c" "/Users/ericgallager/lci/lciframework/CMakeFiles/lci.dir/tokenizer.c.o"
+  "/Users/ericgallager/lci/lciframework/unicode.c" "/Users/ericgallager/lci/lciframework/CMakeFiles/lci.dir/unicode.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ lciframework/CMakeFiles/lci.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for lci.
+# This may be replaced when dependencies are built.
